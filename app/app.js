fdb594ea6ff2cfd25ecfd6a3f1de4e801993aae0
grimcoder <papochka12>
2017-07-04 16:40:25 -0700

    kinda finished

--- app/app.js
+++ app/app.js
@@ -4,28 +4,45 @@ var Posts = require("./Posts")
 
 var UserBox = {
     view: function (vnode) {
-        return m("span", {class: 'fl w-45 pa2 black '},
-            [ m("img", {src: vnode.attrs.url } )]
-        )}
+
+        //<div class="box fl pa1 bg-black ba bw3 br4 ">
+        //    <div class="element " >
+        //        <div style="background:url(https://sandbox-cdn.cosmunity.com/59597afba370ad00586a14ad/59597b15a370ad00586a1a5d-250-508.png) no-repeat top right;background-size: cover;" class="elementLow bg-red">
+        //            <img src="img/buttonOrange.png" class="button"  />
+        //            <div class="white username f2-l">Username</div>
+        //        </div>
+        //    </div>
+        //</div>
+
+
+        return m("div", {class: 'box fl pa1 bg-black ba bw3 br4'},
+            [
+                m("div", {
+                    style: 'background:url(' + vnode.attrs.url + ') no-repeat top right;background-size: cover;',
+                    class: 'elementLow bg-red'
+                }, [
+                    m("img", {src: 'img/buttonOrange.png', class: 'button'}),
+                    m("div", {class: 'white username f5-m'}, vnode.attrs.username.substr(0, 12))
+                ])
+            ]
+        )
+    }
 }
 
 var Main = {
     init: Posts.loadList(),
-    view: function() {
-
+    view: function () {
         var posts = Posts.list;
-        var users = ['59597afba370ad00586a14ad','59597b08a370ad00586a1613'] // users ids that have userpics
-        var owners = users.map((u)=>{
-            var tmp = posts.filter((p)=>{return p.ownerId == u});
-            return tmp.length > 0 ? tmp[0] : undefined;
-
-        }).filter((i)=>{return i});
-
-        return  m(".user-list", owners.map(function(user) {
-            return  [
-                m(UserBox, {url: user.owner.profilePhoto.sizes[0].url, username: user.owner.username})
-            ]
-        }))
+        var owners = posts.slice(0, 2);
+        return  m('div', [
+            m("div", owners.map(function (user) {
+                return [
+                    m(UserBox, {url: user.owner.profilePhoto.sizes[0].url, username: user.owner.username}),
+                ]
+            })),
+            m('div', {class: 'smallbox fl ba br4 '}, [
+                m('div', {class: 'element'})
+            ])])
     }
 }
 
